cfe5a4220738829822ce81921e5df4acfd3aad84
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-04-18 15:16:46 +0100

    Made the site!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+	//@ts-nocheck
+	import { Upload } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+
+	let files = $state();
+	let canvas;
+	let downloadURL = $state();
+	let format = $state('jpeg');
+	let displayImage = $state('');
+	let quality = $state();
+	async function setImage() {
+		displayImage = '';
+		downloadURL = '';
+		if (files.length > 0) {
+			let file = files[0];
+			console.log('File recieved. :D');
+
+			displayImage = URL.createObjectURL(file);
+		}
+	}
+
+	function convertImage() {
+		downloadURL = '';
+
+		let file = files[0];
+		let img = new Image();
+		img.src = URL.createObjectURL(file);
+		img.onload = () => {
+			const ctx = canvas.getContext('2d');
+			canvas.width = img.width;
+			canvas.height = img.height;
+			ctx.drawImage(img, 0, 0);
+			downloadURL = canvas.toDataURL(`image/${format}`);
+			console.log('Exported as', `image/${format}`);
+			if (!downloadURL.startsWith(`data:image/${format}`)) {
+				console.log('Export failed!');
+				alert(
+					'File type is unsupported on this browser. Please switch browsers or try another format.'
+				);
+				displayImage = '';
+				downloadURL = '';
+			}
+		};
+		files = null;
+	}
+</script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" />
+<link
+	href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+	rel="stylesheet"
+/>
+
+<div class="centerdiv">
+	<h1>Convee</h1>
+	<a class="info" href="info">Info</a>
+	<div class="divider"></div>
+	<div class="bubble">
+		<label>
+			<input type="file" bind:files onchange={(e) => setImage(e)} />
+			<Upload class="icon" />
+		</label>
+		<div class="divider"></div>
+		{#if files && files.length >= 1}
+			<img src={displayImage} />
+
+			<select bind:value={format}>
+				<option value="jpeg">JPEG</option>
+				<option value="png">PNG</option>
+				<option value="webp">WebP (smallest file)</option>
+			</select>
+
+			<button onclick={convertImage} class="convert">Convert!</button>
+		{/if}
+		{#if !displayImage}
+			<p>Add an image to get started!</p>
+		{/if}
+		<canvas bind:this={canvas}></canvas>
+
+		{#if downloadURL}
+			<p>Original:</p>
+			<img src={displayImage} />
+			<p>Converted: (.{format})</p>
+			<img src={downloadURL} alt />
+
+			<a class="fancyLink" href={downloadURL} download="ConveeImage">Download Converted Image</a>
+		{/if}
+	</div>
+</div>
---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,24 @@
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" />
+<link
+	href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+	rel="stylesheet"
+/>
+
+<div class="centerdiv">
+	<h1>About Convee</h1>
+	<a class="info" href="/">Back</a>
+	<div class="divider"></div>
+	<p>
+		Convee is a secure, open source image converter. It converts your images fully on-device. If you
+		are curious, the code is available at the github page <a
+			class="normalLink"
+			href="https://github.com/CapyGamesXD/Convee">here</a
+		>.
+		<br />
+		Other interesting projects I've made: <br />
+		<a href="https://lurn.capydesigns.com">LurnAI</a>
+	</p>
+	<div class="divider"></div>
+	<a href="https://lurn.capydesigns.com">Capii</a>
+</div>
